d63cac72cbdab488cc9623927ffa3358fd2be1c8
NarcissusTu <763030205@qq.com>
2026-01-03 15:56:46 +0800

    add component for codecompaion but have some bug

---
+++ nvim/.config/nvim/lua/utils/code-process.lua
@@ -0,0 +1,58 @@
+local M = {}
+
+M.processing = false
+M.spinner_index = 1
+
+local spinner_symbols = {
+  " ⠋",
+  " ⠙",
+  " ⠹",
+  " ⠸",
+  " ⠼",
+  " ⠴",
+  " ⠦",
+  " ⠧",
+  " ⠇",
+  " ⠏",
+}
+
+local spinner_symbols_len = 10
+
+function M:init()
+  local group = vim.api.nvim_create_augroup("CodeCompanionHooks", {})
+
+  vim.api.nvim_create_autocmd({ "User" }, {
+    pattern = "CodeCompanionRequest*",
+    group = group,
+    callback = function(request)
+      if request.match == "CodeCompanionRequestStarted" then
+        self.processing = true
+        vim.g.codecompanion_process = true
+      elseif request.match == "CodeCompanionRequestFinished" then
+        self.processing = false
+        vim.defer_fn(function()
+          vim.g.codecompanion_process = false
+        end, 5000)
+      end
+    end,
+  })
+end
+
+-- Function that runs every time statusline is updated
+function M:update_status()
+  if self.processing then
+    vim.defer_fn(function()
+      -- vim.cmd.redrawstatus()
+      self.spinner_index = (self.spinner_index % spinner_symbols_len) + 1
+    end, 50)
+    return spinner_symbols[self.spinner_index] .. " Wait"
+  else
+    return " SUCCESS"
+  end
+end
+
+function M:is_process()
+  return self.processing
+end
+
+return M
