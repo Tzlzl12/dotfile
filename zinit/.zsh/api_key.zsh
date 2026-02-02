db0febc272e6793ce1e2cb05973e7da7ad15b19f
NarcissusTu <763030205@qq.com>
2026-02-02 15:38:18 +0800

    [zsh]disable some api, and export bun

--- zinit/.zsh/api_key.zsh
+++ zinit/.zsh/api_key.zsh
@@ -1,5 +1,5 @@
-export GEMINI_API_KEY=$(pass api/gemini 2>/dev/null)
-export OPENROUTER_API_KEY=""
+# export GEMINI_API_KEY=$(pass api/gemini 2>/dev/null)
+# export OPENROUTER_API_KEY=""
 export DEEPSEEK_API_KEY=$(pass api/deepseek 2>/dev/null)
 export MODELSCOPE_API_KEY=$(pass api/modelscope 2>/dev/null)
 export CONTEXT7_API_KEY=$(pass api/context7 2>/dev/null)
--- zinit/.zsh/export.zsh
+++ zinit/.zsh/export.zsh
@@ -19,7 +19,7 @@ path=(
     "$HOME/.local/bin"
     "$HOME/.local/share/nvim/mason/bin"
     "$HOME/.rustup/toolchains/esp/riscv32-esp-elf/esp-15.2.0_20250920/riscv32-esp-elf/bin/"
-    # "$HOME/.bun/bin"
+    "$HOME/.bun/bin"
     "$HOME/.cargo/bin"
     # "$HOME/.dotnet"
     # "$HOME/tools/renode"
